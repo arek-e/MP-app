1363b87432100c123d2a3bb6aa12ce6ce07df220
Alexander Eklund <alexeklund460@gmail.com>
2022-11-28 16:40:04 +0900

    - Added support for custom view markers.

--- frontend/app/src/main/java/com/example/frontend/MapsActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/MapsActivity.kt
@@ -1,11 +1,8 @@
 package com.example.frontend
 
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.drawable.toBitmap
-import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
+import androidx.core.content.ContextCompat
 import com.example.frontend.databinding.ActivityMapsBinding
 import com.example.frontend.place.Place
 import com.example.frontend.place.PlacesReader
@@ -13,8 +10,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 
 
@@ -34,34 +31,44 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
+        mapFragment?.getMapAsync {
+            it.setOnMapLoadedCallback {
+                val bounds = LatLngBounds.builder()
+                places.forEach { bounds.include(it.latLng) }
+                it.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+            }
 
-        mapFragment?.getMapAsync { addMarkers(mMap) }
-
-
+            addMarkers(it)
+            it.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //val sydney = LatLng(-34.0, 151.0)
+        //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //mMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
     }
+
+    private val trashIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.purple_500)
+        BitmapHelper.vectorToBitmap(this, R.drawable.ic_baseline_restore_from_trash_24, color)
+    }
+
 
     /**
      * Adds markers to the map. These markers won't be clustered.
      */
     private fun addMarkers(googleMap: GoogleMap) {
-        val vectorDrawable: Drawable? = VectorDrawableCompat.create(resources, R.drawable.ic_baseline_restore_from_trash_24, baseContext.theme)
-        val trashIcon: Bitmap? = vectorDrawable?.toBitmap()
         places.forEach { place ->
             val marker = googleMap.addMarker(
                 MarkerOptions()
                     .title(place.name)
                     .position(place.latLng)
-                    .icon(BitmapDescriptorFactory.fromBitmap(trashIcon!!))
+                    .icon(trashIcon)
             )
             // Set place as the tag on the marker object so it can be referenced within
             // MarkerInfoWindowAdapter
