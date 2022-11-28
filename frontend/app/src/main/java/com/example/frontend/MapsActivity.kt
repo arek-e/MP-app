0758671415acee914948f1daa52913b93edc2981
Alexander Eklund <alexeklund460@gmail.com>
2022-11-28 12:44:33 +0900

    - Support for adding marker from json file implemented

--- frontend/app/src/main/java/com/example/frontend/MapsActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/MapsActivity.kt
@@ -1,28 +1,44 @@
 package com.example.frontend
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
+import com.example.frontend.databinding.ActivityMapsBinding
+import com.example.frontend.place.Place
+import com.example.frontend.place.PlacesReader
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.frontend.databinding.ActivityMapsBinding
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private val binding by lazy { ActivityMapsBinding.inflate(layoutInflater) }
 
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        mapFragment?.getMapAsync { addMarkers(mMap) }
+
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -32,5 +48,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    /**
+     * Adds markers to the map. These markers won't be clustered.
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        val vectorDrawable: Drawable? = VectorDrawableCompat.create(resources, R.drawable.ic_baseline_restore_from_trash_24, baseContext.theme)
+        val trashIcon: Bitmap? = vectorDrawable?.toBitmap()
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+                    .icon(BitmapDescriptorFactory.fromBitmap(trashIcon!!))
+            )
+            // Set place as the tag on the marker object so it can be referenced within
+            // MarkerInfoWindowAdapter
+            marker?.tag = place
+        }
+
     }
 }
