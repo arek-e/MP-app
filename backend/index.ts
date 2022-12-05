a2a28283517de8b2ca62a1869b53732aefd8804a
Lana Sirwan A <lana.abno20@gmail.com>
2022-12-05 04:50:27 +0100

    Creating specified endpoints

--- backend/index.ts
+++ backend/index.ts
@@ -7,53 +7,51 @@ const app: Application = express();
 const port = 3000;
 
 //Body parsing Middleware aka plugins till servern
+//Creating all functions that the frontend needs
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.get(
-    "/",
-    async (req: Request, res: Response): Promise<Response> => {
-        return res.status(200).send({
-            message: "Hello World!"
-        });
-    }
-);
-app.get('/feed', async (req: Request, res: Response) => {
-    const posts = await prisma.post.findMany({
-        where: { published: true},
-        include: {author: true},
-    })
-    res.json(posts)
-});
+//Creating the first endpoint
+//make this authenticated so only admin can get all the users
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
+    //serializing the data, so we can send the data
+    res.json(users)
+})
+//Getting all the trashbins on the mao
+app.get('/trashbins', async(req, res) => {
+    const trashbins = await prisma.trashbin.findMany()
+    res.json(trashbins)
+})
 
-app.post('/post', async (req: Request, res: Response) => {
-const { title, content, authorEmail } = req.body
-const result = await prisma.post.create({
-    data: {
-        title,
-        content,
-        published: false,
-        author: { connect: { email: authorEmail } },
-    },
-})
-res.json(result)
-});
-app.delete('/post/:id', async (req: Request, res: Response) => {
-    const { id } = req.params
-    const post = await prisma.post.delete({
+//Getting the trashbins depending on the wastetype
+app.get('/trashbin/wastetype/:type', async(req, res) => {
+    const trashbinsWithGlass = await prisma.trashbin.findMany({
         where: {
-            id: Number(id),
-        },
+            wastetypes: {
+                some: {
+                    wastetype: {
+                        wastetype: req.params.type
+                    }
+                }
+            }
+        }
     })
-    res.json(post)
+    res.json(trashbinsWithGlass)
 })
-app.put('/publish/:id', async (req: Request, res: Response) => {
-    const { id } = req.params
-    const post = await prisma.post.update({
-        where: { id: Number(id) },
-        data: { published: true },
+//Creating a new user
+app.post('/new/user', async(req, res) =>{
+    const newUser = await prisma.user.create({
+        data:{...req.body},
     })
-    res.json(post)
+    res.json(newUser)
+})
+//creating a new bin
+app.post('/new/trashbin', async(req, res) =>{
+    const newTrashbin = await prisma.trashbin.create({
+        data:{...req.body},
+    })
+    res.json(newTrashbin)
 })
 
 try{
